7a995f7616028281068197f6da245fc2faf831ff
220530AM352 <abc@naver.com>
2022-06-08 15:22:34 +0900

    commit

---
+++ javaWorkspace/javaPrj00/src/codeup/Problem1352.java
@@ -0,0 +1,81 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Problem1352 {
+
+	public static void main(String[] args) {
+		
+		// n입력받고, 정사각형 모양으로 * 출력
+		
+		Scanner sc = new Scanner(System.in);
+		int n =sc.nextInt();
+		
+		for(int i = 0 ; i < n; i++) {
+			for(int j = 0 ; j < n; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+
+	}//main
+
+}//class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
