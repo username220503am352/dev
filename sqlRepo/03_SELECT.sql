bfb5ffbf46ab0aba03678d959f2cb5f4b039ba0d
220530AM352 <abc@naver.com>
2022-07-06 15:23:38 +0900

    SQL

--- sqlRepo/03_SELECT.sql
+++ sqlRepo/03_SELECT.sql
@@ -129,6 +129,261 @@ WHERE SAL >= 1000;
 
 -- 2. EMP 테이블에서 급여가 1500 이하인 사원의 이름, 급여, 부서코드 조회
 
+/*
+    <논리 연산자>
+        AND
+        OR
+*/
+
+-- EMP 테이블에서, 부서코드가 30 이면서 급여가 1000 이상인 사원 조회
+SELECT *
+FROM EMP
+WHERE DEPTNO=30
+AND SAL >= 1000
+;
+
+-- EMP 테이블에서 급여가 2000 이상이면서, JOB이 MANAGER 인 사원의 모든 칼럼 조회
+SELECT *
+FROM EMP
+WHERE SAL >= 2000
+AND JOB = 'MANAGER';
+-- EMP 테이블에서 급여가 2000 이상이면서 4000 이하인 사원의 사번, 사원명, 부서코드, 급여 조회
+SELECT 
+    EMPNO
+    , ENAME
+    , DEPTNO
+    , SAL
+FROM 
+    EMP
+WHERE SAL >= 2000
+AND SAL <= 4000
+;
+
+/*
+    <BETWEEN AND>
+        [문법]
+            WHERE 비교대상칼럼 BETWEEN A AND B
+            
+        - WHERE 절에서 사용되는 구문으로 범위에 대한 조건을 제시할 때 사용
+        - 비교대상칼럼 값이 A와 B 사이인 경우 TRUE 를 리턴함
+*/
+
+-- EMP 테이블에서 급여가 2000 이상이면서 4000 이하인 사원의 사번, 사원명, 부서코드, 급여 조회
+SELECT 
+    EMPNO
+    , ENAME
+    , DEPTNO
+    , SAL
+FROM 
+    EMP
+WHERE SAL BETWEEN 2000 AND 4000
+--AND
+--DEPTNO BETWEEN 20 AND 40
+;
+
+-- EMP 테이블에서 급여가 (2000 이상이면서 4000 이하)가 아닌 사원의 사번, 사원명, 부서코드, 급여 조회
+SELECT 
+    EMPNO
+    , ENAME
+    , DEPTNO
+    , SAL
+FROM 
+    EMP
+WHERE NOT SAL BETWEEN 2000 AND 4000 --NOT 은 칼럼앞 또는 BETWEEN 앞 에 작성 가능
+;
+
+-- EMP 테이블에서, 입사일이 81/01/01 ~ 81/12/31 이 아닌 사원의 모든 칼럼 조회
+SELECT *
+FROM EMP
+WHERE NOT HIREDATE BETWEEN '81/01/01' AND '81/12/31'
+ORDER BY HIREDATE
+;
+
+-- OR 연산자
+SELECT *
+FROM EMP
+WHERE SAL < 1000
+OR NOT SAL > 4000
+;
+
+/*
+    <LIKE>
+        [문법]
+            WHERE 비교칼럼 LIKE '패턴';
+            
+        - 비교하려는 칼럼이 지정된 특정 패턴에 만족할 경우 TRUE 리턴
+        - 특정 패턴에는 '%', '_' 를 와일드 카드로 사용 가능
+            % : 0글자 이상
+                EX ) 비교칼럼 LIKE '안녕%' => 비교칼럼 값 중 '안녕' 으로 시작하는 모든 행을 조회
+                     비교칼럼 LIKE '%안녕' => 비교칼럼 값 중 '안녕' 으로 끝나는 모든 행을 조회
+                     비교칼럼 LIKE '%안녕%' => 비교칼럼 값 중 '안녕' 을 포함하는 모든 행을 조회
+            _ : 1글자
+                EX ) 비교칼럼 LIKE '_안녕' => 비교칼럼 값 중 '안녕'앞에 한글자가 오는 행을 조회
+                     비교칼럼 LIKE '안녕_' => 비교칼럼 값 중 '안녕'뒤에 한글자가 오는 행을 조회
+                     비교칼럼 LIKE '안녕__' => 비교칼럼 값 중 '안녕'뒤에 두글자가 오는 행을 조회
+*/
+
+-- EMP 테이블에서 이름이 J 로 시작하는 사원의 사번,사원명,부서코드 조회
+SELECT EMPNO, ENAME, DEPTNO
+FROM EMP
+WHERE ENAME LIKE 'J%';
+
+-- EMP 테이블에서 이름이 N 으로 끝나는 사원의 사번,사원명,부서코드 조회
+SELECT EMPNO, ENAME, DEPTNO
+FROM EMP
+WHERE ENAME LIKE '%N';
+
+-- EMP 테이블에서 이름에 E 가 포함되는 사원의 사번,사원명,부서코드 조회
+SELECT EMPNO, ENAME, DEPTNO
+FROM EMP
+WHERE ENAME LIKE '%E%';
+
+-- EMP 테이블에서 이름이 5글자인 사원의 사번,사원명 조회
+SELECT EMPNO, ENAME
+FROM EMP
+WHERE ENAME LIKE '_____';
+
+-- EMP 테이블에서 사원번호의 두번째 자리가 '5' 인 사원의 사번,사원명 조회
+SELECT EMPNO, ENAME
+FROM EMP
+WHERE EMPNO LIKE '_5%';
+
+-------------------------- 실습 -------------------
+-- 1. EMP 테이블에서 이름의 첫글자가 S 가 아닌 사원의 사번,이름 조회
+SELECT EMPNO, ENAME
+FROM EMP
+WHERE ENAME NOT LIKE 'S%';
+
+
+
+/*
+    <IS NULL / IS NOT NULL>
+        [문법]
+            WHERE 비교칼럼 IS [NOT] NULL;
+            
+        - 칼럼 값에 NULL 이 있을 경우 NULL 값 비교에 사용됨
+        - IS NULL : 비교칼럼 값이 NULL 인 경우 TRUE
+        - IS NOT NULL : 비교칼럼 값이 NULL 이 아닌 경우 TRUE
+*/
+
+-- EMP 테이블에서 COMM 이 NULL 인 사원의 모든 칼럼 조회
+SELECT *
+FROM EMP
+WHERE COMM IS NULL;
+
+-- EMP 테이블에서 COMM 이 NULL 이 아닌 사원의 모든 칼럼 조회
+SELECT *
+FROM EMP
+WHERE COMM IS NOT NULL;
+
+/*
+    <IN>
+        [문법]
+            WHERE 비교칼럼 IN (값, 값, 값, 값 ... 값);
+            
+        - 값 목록 중 일치하는 값이 있을 경우 TRUE 리턴    
+*/
+
+-- EMP 테이블 에서, JOB 이 SALESMAN 이거나 MANAGER 인 사원의 모든 칼럼 조회
+SELECT *
+FROM EMP
+WHERE JOB IN ('SALESMAN', 'MANAGER')
+;
+
+/*
+    <연산자 우선순위>
+        0. ()
+        1. 산술 연산자
+        2. 연결 연산자
+        3. 비교 연산자
+        4. IS NULL , LIKE, IN
+        5. BETWEEN AND
+        6. NOT
+        7. AND
+        8. OR
+*/
+
+/*
+    <ORDER BY>
+        SQL 마지막에 추가하여 정렬 가능
+        ASC , DESC 으로 오름차순 내림차순 정렬 가능
+*/
+
+-- EMP 테이블의 모든 데이터 조회
+SELECT *
+FROM EMP
+--ORDER BY COMM
+--ORDER BY COMM ASC
+--ORDER BY COMM DESC
+--ORDER BY COMM ASC NULLS FIRST
+--ORDER BY COMM ASC NULLS LAST
+ORDER BY DEPTNO, SAL
+;
+
+-- EMP 테이블의 EMPNO, ENAME, HIREDATE 데이터 조회
+SELECT EMPNO 사번, ENAME 사원명, HIREDATE 입사일
+FROM EMP
+--ORDER BY HIREDATE
+ORDER BY 입사일
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ sqlRepo/04_함수.sql
@@ -0,0 +1,369 @@
+-- 함수
+/*
+    <함수>
+        칼럼값을 읽어서 계산 결과를 반환
+        - 단일행 함수 : N개의 값을 읽고 N개의 결과 리턴 (매 행 마다, 함수 실행 -> 결과 반환)
+        - 그룹   함수 : N개의 값을 읽고 1개의 결과 리턴 (하나의 그룹별로 함수 실행 -> 결과 반환)
+        
+        SELECT 절에 단일행 함수와 그룹 함수를 함게 사용할 수 없음 (결과 행의 갯수가 다르므로)
+        함수를 기술할 수 있는 위치는 SELECT, WHERE ,ORDER BY, HAVING 절에 기술할 수 있다.
+*/
+
+-------------------단일행 함수-------------------------------
+
+/*
+    <문자 관련 함수>
+    1) LENGTH / LENGTHB
+    - LENGTH(칼럼|'문자열') : 글자 수 반환
+    - LENGTHB(칼럼|'문자열') : 글자의 바이트 수 반환
+      한글 : 3BYTE
+      영문자, 숫자, 특수문자 : 1BYTE
+    
+    * DUAL 테이블
+    - SYS 사용자가 소유하는 테이블
+    - SYS 사용자가 소유하지만, 모든 사용자가 접근이 가능하다
+    - 한 행, 한 칼럼을 가지고 있는 DUMMY 테이블이다.
+    - 사용자가 함수를 사용할 때 임시로 사용하는 테이블
+*/
+
+SELECT * FROM DUAL;
+
+SELECT LENGTH('안녕') , LENGTHB('안녕')
+FROM DUAL;
+
+SELECT 
+        LENGTH(EMPNO)
+    ,   LENGTHB(ENAME)
+FROM EMP;
+
+/*
+    2) INSTR
+        - INSTR(칼럼|'문자열' , 찾을값 [, POSITION[, OCCURENCE]] )
+        - 지정한 위치부터 지정된 숫자 번째로 나타나는 문자의 시작 위치를 반환
+*/
+
+SELECT INSTR('AABAACAABBAA', 'B') FROM DUAL;            --3 : B의 위치
+SELECT INSTR('AABAACAABBAA', 'B' , 1) FROM DUAL;        --3 : B의 위치
+SELECT INSTR('AABAACAABBAA', 'B' , 1, 2) FROM DUAL;     --9 : B의 위치 (2번째 B)
+SELECT INSTR('AABAACAABBAA', 'B' , -1) FROM DUAL;       --10: B의 위치 (뒤에서 첫번째)
+SELECT INSTR('AABAACAABBAA', 'B' , -1, 3) FROM DUAL;    --3 : B의 위치 (뒤에서 세번째)
+
+
+/*
+    3) LPAD / RPAD
+        - LPAD|RPAD(칼럼|문자열 , 길이(바이트)[, 문자])
+        - 제시된 칼럼|문자값 에 임의의 문자를 왼쪽 또는 오른쪽에 덧붙여 최종 N 길이 만큼의 문자열 반환
+        - 문자에 대해 통일감 있게 표시하고자 할 때 사용한다.
+*/
+
+-- 10만큼의 길이 중 ENAME 값은 오른쪽 정렬 , 왼쪽은 공백으로 채우기
+SELECT LPAD(ENAME, 10)
+FROM EMP;
+
+-- 10만큼의 길이 중 ENAME 값은 오른쪽 정렬 , 왼쪽은 '#' 으로 채우기
+SELECT LPAD(ENAME, 10, '#')
+FROM EMP;
+
+-- 10만큼의 길이 중 ENAME 값은 왼쪽 정렬 , 오른쪽은 공백으로 채우기
+SELECT RPAD(ENAME, 10)
+FROM EMP;
+
+SELECT RPAD(ENAME, 10, '@')
+FROM EMP;
+
+
+/*
+    4) LTRIM / RTRIM
+        - LTRIM/RTRIM(칼럼|문자값[, 제거문자])
+        - 문자열의 왼쪽 혹은 오른쪽에서 제거하고자 하는 문자들을 찾아서 제거한 결과 반환
+        - 제거하고자 하는 문자값을 생략 시 기본값으로 공백 제거
+*/
+
+SELECT LTRIM('            HELLO              ') FROM DUAL;
+SELECT RTRIM('            HELLO              ') FROM DUAL;
+
+SELECT LTRIM('00000123000000' , 0) FROM DUAL;
+SELECT RTRIM('00000123000000' , 0) FROM DUAL;
+
+SELECT LTRIM('12345' , 3) FROM DUAL;
+SELECT RTRIM('12345' , 4) FROM DUAL;
+
+/*
+    5) TRIM
+        - TRIM([LEADING|TRAILING|BOTH] 제거문자 FROM 칼럼|문자)
+        - 문자값 앞/뒤/양쪽 지정한 문자를 제거하고 나머지 반환
+        - 제거하고자 하는 문자 생략 시 , 공백을 제거
+*/
+
+SELECT '   안녕   ' FROM DUAL;
+SELECT TRIM('   안녕   ') FROM DUAL;
+
+SELECT TRIM('Z' FROM 'ZZZHELLOZZZ') FROM DUAL;
+SELECT TRIM(LEADING 'Z' FROM 'ZZZHELLOZZZ') FROM DUAL;
+SELECT TRIM(TRAILING 'Z' FROM 'ZZZHELLOZZZ') FROM DUAL;
+
+
+/*
+    6) SUBSTR
+        - SUBSTR(칼럼|문자값, POSITION [,LENGTH])
+        - 문자 데이터에서 지정한 위치부터 지정한 갯수만큼의 문자열 추출해서 반환
+*/
+
+SELECT 'HELLO WORLD' FROM DUAL;
+SELECT SUBSTR('HELLO WORLD', 3) FROM DUAL;
+SELECT SUBSTR('HELLO WORLD', 3, 5) FROM DUAL;
+SELECT SUBSTR('HELLO WORLD', -5) FROM DUAL;
+SELECT SUBSTR('HELLO WORLD', -5, 3) FROM DUAL;
+
+
+/*
+    7) LOWER / UPPER / INITCAP
+*/
+
+SELECT 'Hello World !' FROM DUAL;
+
+SELECT LOWER('Hello World !') FROM DUAL;
+SELECT UPPER('Hello World !') FROM DUAL;
+SELECT INITCAP('abc!') FROM DUAL;
+
+/*
+    8) CONCAT
+        - CONCAT(칼럼|문자값, 칼럼|문자값)
+        - 문자데이터 두개를 받아서 합쳐줌
+*/
+
+SELECT CONCAT('AAA','BBB')
+FROM DUAL;
+
+/*
+    9) REPLACE
+        - REPLACE(칼럼|문자값 , TARGET, STR)
+        - 칼럼 또는 문자값에서 TARTGET 을 STR 로 변경하여 반환
+*/
+
+SELECT '서울시 강남구 역삼동' FROM DUAL;
+
+SELECT REPLACE('서울시 강남구 역삼동', '강남구', '강북구') FROM DUAL;
+SELECT REPLACE('서울시 강남구 역삼동', '서울시', '서울특별시') FROM DUAL;
+
+SELECT '안녕하세요 안녕히가세요' FROM DUAL;
+SELECT REPLACE('안녕하세요 안녕히가세요', '안녕', '') FROM DUAL;
+
+
+--------------------------------------
+/*
+    1) ABS
+        - ABS(NUMBER)
+        - 절대값 구하는 함수
+*/
+
+SELECT ABS(-7) FROM DUAL;
+
+/*
+    2) MOD
+        - MOD(NUMBER , NUMBER)
+*/
+
+-- SELECT 10%3 FROM DUAL; --에러발생
+SELECT MOD(10,3) FROM DUAL;
+
+/*
+    3) ROUND
+        - ROUND(NUMBER[, 위치])
+        - 반올림
+        - 위치 : 기본값 0
+*/
+
+SELECT ROUND(123.456) FROM DUAL;
+SELECT ROUND(123.456, 1) FROM DUAL;
+SELECT ROUND(123.456, 4) FROM DUAL;
+SELECT ROUND(123.456, -1) FROM DUAL;
+SELECT ROUND(123.456, -2) FROM DUAL;
+SELECT ROUND(123.456, -3) FROM DUAL;
+SELECT ROUND(789.456, -3) FROM DUAL;
+
+/*
+    4) CEIL
+        - CEIL(NUMBER)
+        - 소수점 아래 올림
+*/
+
+SELECT CEIL(123.456) FROM DUAL;
+
+/*
+    5) FLOOR
+        - 소수점 아래 버림
+*/
+
+SELECT FLOOR(123.987) FROM DUAL;
+
+/*
+    6) TRUNC
+        - 위치 지정하여 버림
+        - 음수로 지정도 가능
+*/
+
+SELECT TRUNC(123.456) FROM DUAL;
+SELECT TRUNC(123.456, 1) FROM DUAL;
+SELECT TRUNC(123.456, 2) FROM DUAL;
+SELECT TRUNC(123.456, -2) FROM DUAL;
+
+
+--------------------------------------------------------------
+/*
+    날짜 관련 함수
+    
+    1) SYSDATE
+        - 시스템의 현재 날짜와 시간 반환
+*/
+
+SELECT SYSDATE FROM DUAL;
+
+/*
+    2) MONTHS_BETWEEN
+        - 두 날짜 사이의 개월수를 리턴
+        - 리턴값은 NUMBER
+*/
+SELECT ENAME, FLOOR(MONTHS_BETWEEN(SYSDATE ,HIREDATE)) 근무개월수 , HIREDATE 입사일
+FROM EMP;
+
+/*
+    3) ADD_MONTHS
+        - 개월수를 더해줌
+        - 리턴값 : DATE
+*/
+
+SELECT ADD_MONTHS(SYSDATE, 6) FROM DUAL;
+SELECT ADD_MONTHS('2002/12/31', 6) FROM DUAL;
+
+/*
+    NEXT_DAY
+        [문법]
+            NEXT_DAY(DATE, 요일(문자|숫자))
+        - DATE 기준 가장 가까운 다음 요일 리턴
+*/
+
+SELECT SYSDATE, NEXT_DAY(SYSDATE, '금요일') FROM DUAL;
+SELECT SYSDATE, NEXT_DAY(SYSDATE, '금') FROM DUAL;
+SELECT SYSDATE, NEXT_DAY(SYSDATE, 6) FROM DUAL;
+--SELECT SYSDATE, NEXT_DAY(SYSDATE, 'FRIDAY') FROM DUAL; -- 언어 설정 변경해야 됨
+
+ALTER SESSION SET NLS_LANGUAGE = AMERICAN; -- 언어 변경
+SELECT SYSDATE, NEXT_DAY(SYSDATE, 'FRIDAY') FROM DUAL;
+SELECT SYSDATE, NEXT_DAY(SYSDATE, 'FRI') FROM DUAL;
+SELECT SYSDATE, NEXT_DAY(SYSDATE, 6) FROM DUAL;
+SELECT SYSDATE, NEXT_DAY(SYSDATE, '금') FROM DUAL;
+
+ALTER SESSION SET NLS_LANGUAGE = KOREAN; -- 언어 변경
+
+
+/*
+    LAST_DAY
+         - 해당 달의 마지막 날짜 리턴
+*/
+
+SELECT LAST_DAY(SYSDATE) FROM DUAL;
+SELECT LAST_DAY('22/08/08') FROM DUAL;
+SELECT LAST_DAY('22/12/01') FROM DUAL;
+
+/*
+    EXTRACT
+        [문법]
+        - EXTRACT(YEAR | MONTH | DAY FROM DATE)
+        - 리턴타입은 NUMBER
+*/
+
+SELECT EXTRACT(YEAR FROM SYSDATE) FROM DUAL;
+SELECT EXTRACT(MONTH FROM SYSDATE) FROM DUAL;
+SELECT EXTRACT(DAY FROM SYSDATE) FROM DUAL;
+
+-- 날짜 포맷 변경
+SELECT SYSDATE FROM DUAL;
+
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD HH:MI:SS';
+ALTER SESSION SET NLS_DATE_FORMAT = 'RR/MM/DD';
+
+---------------------------------------------------------------------------
+
+/*
+    형변환 함수
+    
+    1) TO_CHAR
+*/
+
+-- 숫자 -> 문자
+SELECT TO_CHAR(1234) FROM DUAL;
+SELECT TO_CHAR(1234 , '999999') FROM DUAL; -- 6칸의 공간 확보, 빈칸은 공백으로 채움
+SELECT TO_CHAR(1234 , '000000') FROM DUAL; -- 6칸의 공간 확보, 빈칸은 0 으로 채움
+SELECT TO_CHAR(1234 , 'L999999') FROM DUAL; -- 현재 설정된 나라의 화폐단위
+SELECT TO_CHAR(1234 , '$999999') FROM DUAL;
+SELECT TO_CHAR(123456789 , 'L999,999,999,999') FROM DUAL;
+
+-- 사원 테이블 급여 조회
+SELECT EMPNO, ENAME, TO_CHAR(SAL , 'L99,999,999')
+FROM EMP
+ORDER BY SAL
+;
+
+-- 날짜 -> 문자
+SELECT SYSDATE FROM DUAL;
+SELECT TO_CHAR(SYSDATE) FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD HH:MI:SS') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD HH:MI:SS AM') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'MON DY, YYYY') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'MON DAY, YYYY') FROM DUAL;
+SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD(DY)') FROM DUAL;
+
+
+/*
+    2) TO_DATE
+*/
+
+-- 숫자 -> 날짜
+SELECT TO_DATE(20220706) FROM DUAL;
+
+-- 문자 -> 날짜
+SELECT TO_DATE('20220706') FROM DUAL;
+SELECT TO_DATE('2022/07/06') FROM DUAL;
+SELECT TO_DATE('2022-07-06') FROM DUAL;
+SELECT TO_DATE('2022.07.06') FROM DUAL;
+
+
+/*
+    3) TO_NUMBER
+*/
+
+SELECT TO_NUMBER('123456789') FROM DUAL;
+SELECT '111' + '222' FROM DUAL; -- 자동으로 숫자 타입으로 형변환 후 연산처리
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
